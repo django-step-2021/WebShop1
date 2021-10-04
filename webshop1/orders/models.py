6ca6c6c4a9020c8c8694c96ace0145a3f595d804
devoden <devohutsdv@gmail.com>
2021-10-04 17:25:44 +0300

    Order migration to orders table

--- webshop1/orders/models.py
+++ webshop1/orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+
+    product = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+    producer = models.CharField(max_length=100)
+    count = models.IntegerField()
+    price = models.FloatField()
+
+    def __str(self) -> str:
+        return str(self.title)
