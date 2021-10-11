1cc71404fb5580f4ab61f7891624e00af25f1a5b
devoden <devohutsdv@gmail.com>
2021-10-11 22:18:12 +0300

    Catalog section

--- webshop1/account/urls.py
+++ webshop1/account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import register
+from .views import register, entry, sign_out
 
 urlpatterns = [
     path('register', register),
+    path('entry', entry),
+    path('logout', sign_out),
 ]
--- webshop1/account/views.py
+++ webshop1/account/views.py
@@ -37,3 +37,40 @@ def register(request):
         'mess': mess,
         'color': color,
     })
+
+
+def entry(request):
+    if request.method == 'GET':
+        return render(request, 'account/entry.html', context={
+            'page_name': 'Авторизация',
+            'page_app': 'account',
+            'page_view': 'entry',
+        })
+    elif request.method == 'POST':
+        login_x = request.POST.get('login')
+        pass1_x = request.POST.get('pass1')
+
+    # 2 - Проверка подлинности пары логин-пароль ...
+    user = authenticate(request, username=login_x, password=pass1_x)
+    if user is None:
+        mess = 'Пользователь не найден!'
+        color = 'red'
+        return render(request, 'account/report.html', context={
+            'page_name': 'Отчет об авторизации',
+            'page_app': 'account',
+            'page_view': 'report',
+            'mess': mess,
+            'color': color,
+        })
+    else:
+        login(request, user)
+        return redirect('/')
+
+
+def sign_out(request):
+    logout(request)
+    return render(request, 'account/logout.html', context={
+        'page_name': 'Выход',
+        'page_app': 'account',
+        'page_view': 'sign_out',
+    })
