64cd10249d52a69bbdfcf405e38616ba49c87d08
devoden <devohutsdv@gmail.com>
2021-10-18 22:30:45 +0300

    Cart product quanity added

--- webshop1/orders/views.py
+++ webshop1/orders/views.py
@@ -5,5 +5,21 @@ from .models import Order
 
 def ajax_cart(request):
     response = dict()
-    response['test'] = 'Ajax - work!'
+    uid = request.GET.get('uid')
+    pid = request.GET.get('pid')
+
+    Order.objects.create(
+        title=f'Order-{pid}/{uid}',
+        product_id=pid,
+        user_id=uid,
+        status='Ожидание подтверждения'
+    )
+
+    user_orders = Order.objects.filter(user_id=uid)
+    cost = 0
+    for order in user_orders:
+        cost += order.product.price
+
+    response['cost'] = cost
+    response['count'] = len(user_orders)
     return JsonResponse(response)
--- webshop1/static/js/cart.js
+++ webshop1/static/js/cart.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
 
     $('.add-to-cart-btn').click(function() {
+        //
         let productId = $(this).prev().val();
+        let userId = $(this).prev().prev().val();
+        //
+        if (userId == 'None') {
+            alert('Для использования корзины Вы должны авторизоваться!');
+            window.location = '/account/entry';
+        } else {
+            let uid = userId;
+            let pid = productId;
+            $.ajax({
+                url: '/orders/ajax_cart',
+                data: `uid=${uid}&pid=${pid}`,
+                success: function (result) {
+                    alert('Товар будет добавлен в корзину!');
+                    $('#count').text(result.count);
+                }
+            });
+        }
 
-        alert('Select product with ID: ' + productId);
-    })
+    });
 
 });
